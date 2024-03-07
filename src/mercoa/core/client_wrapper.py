52dcaf8fdf0a27d33856e01120023af703a793a4
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-03-07 02:07:41 +0000

    Release v0.3.24

--- src/mercoa/core/client_wrapper.py
+++ src/mercoa/core/client_wrapper.py
@@ -14,7 +14,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "mercoa",
-            "X-Fern-SDK-Version": "v0.3.23",
+            "X-Fern-SDK-Version": "v0.3.24",
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
         return headers
